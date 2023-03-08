db0d871ac088c96dbd3c06c0d2be520e38f0e192
Sean Zweifel <spzweifel@gmail.com>
2023-03-08 18:30:20 -0500

    tried to work on prompts

--- script.js
+++ script.js
@@ -6,29 +6,38 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
-  // this seems to have broken the page because of some unknown error over the > sign
-  const between = (x, 8, 128) , {
-    return: x >= 8 && x <= 128;
-  }
-
-
-
-
   passwordText.value = password;
 
 }
 
+//this function puts the generated password into the textbox
 function generatePassword() {
-  
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 8;
+
+  for (var i=0; i<passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber,randomNumber+1);
+  }
+  document.getElementById("password").value = password;
+  //figure out how to return what I want it to return and not what's typed here
+  //return "#password";
 }
 
+function promptMe(){
+  var userAdjective = prompt("how long is the password?");
+  alert (userAdjective);
+}
 // Add event listener to generate button
-// I think this is on the right track 
 generateBtn.addEventListener("click", writePassword);
-  let text;
-  let person = prompt("Please enter password length: ");
-  if (person == null || person == "") {
-    closeevent = prompt;
-  } else {
-    let person = prompt("Please enter character type: ");
-  }
+let text;
+onclick=let person = prompt("Please enter password length: ");
+if (person == null || person == "") {
+  closeevent = prompt;
+} else {
+  let person = prompt("Please enter character type: ");
+}
+
+ 
+
+  
