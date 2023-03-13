7f3ec760e00452ada64aca607b76506f1ff46808
Sean Zweifel <spzweifel@gmail.com>
2023-03-12 22:54:49 -0400

    attempted to finish the project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Password_Generator
+
+## Description
+In this project, I set out to finish the starter code on a random password generator. My job was to make the code prompt the user for a number between 8 and 128. It would then ask if special characters, and upper and lower case letters would be used. It would then output a random password.
+
+## License
+
+MIT License
+
+## Screenshot
+
+![Alt text](images/Password.png)
--- script.js
+++ script.js
@@ -17,38 +17,22 @@ function generatePassword() {
     alert("Please, enter a value between 8 and 128");
     return;
   }
+
   var uppercase = confirm ("Do you want to use uppercase?");
-  if (uppercase = true) {
-    
-  }
   var lowercase = confirm ("Do you want to use lowercase?");
-  if (lowercase = true) {
-
-  }
   var splchar = confirm ("Do you want to use special characters?");
-  if (splchar = true) {
 
-  }
 
   
   //if/else stuff after this
   //if pwLength (x <= 8 || x >= 128)
-  var chars =
-    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  for (var i = 0; i < pwLength; i++) {
-    //get rid of all this
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
-  }
-  document.getElementById("password").value = password;
+ 
+  
   //figure out how to return what I want it to return and not what's typed here
   //return "#password";
 }
 
-function promptMe() {
-  var userAdjective = prompt("how long is the password?");
-  alert(userAdjective);
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  generatePassword(pwLength, uppercase, lowercase, splchar);
