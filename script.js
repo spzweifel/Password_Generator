4800c1a632298773cf65104181fea2a937dea84a
Sean Zweifel <spzweifel@gmail.com>
2023-08-14 20:53:53 -0400

    fixed the code

--- script.js
+++ script.js
@@ -3,36 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var pwLength = getPasswordLength();
+  if (pwLength !== null) {
+    var uppercase = confirm("Do you want to use uppercase?");
+    var lowercase = confirm("Do you want to use lowercase?");
+    var splchar = confirm("Do you want to use special characters?");
 
-  passwordText.value = password;
+    var password = generatePassword(pwLength, uppercase, lowercase, splchar);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+}
+
+function getPasswordLength() {
+  while (true) {
+    var pwLength = parseInt(
+      prompt(
+        "what length would you like your password? Please, enter a value between 8 and 128"
+      )
+    );
+    if (pwLength >= 8 || pwLength <= 128) {
+      return pwLength;
+    }
+    alert("Please, enter a value between 8 and 128");
+  }
 }
 
 //this function puts the generated password into the textbox
-function generatePassword() {
-  var pwLength = prompt("what length would you like your password? Please, enter a value between 8 and 128");
-  console.log(pwLength);
-  if (pwLength < 8 || pwLength > 128) {
-    alert("Please, enter a value between 8 and 128");
-    return;
+function generatePassword(length, uppercase, lowercase, splchar) {
+  var charset = "";
+  if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (splchar) charset += "!@#$%^&*()";
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
   }
-
-  var uppercase = confirm ("Do you want to use uppercase?");
-  var lowercase = confirm ("Do you want to use lowercase?");
-  var splchar = confirm ("Do you want to use special characters?");
-
-
-  
-  //if/else stuff after this
-  //if pwLength (x <= 8 || x >= 128)
- 
-  
-  //figure out how to return what I want it to return and not what's typed here
-  //return "#password";
+  return password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  generatePassword(pwLength, uppercase, lowercase, splchar);
